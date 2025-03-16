9eb4185ba759db571a6402836f136c5857fd4f8e
felk <de.felix.koenig@gmail.com>
2025-03-16 13:39:41 +0100

    Make MusicCat.Service just start MusicCat.WebService

--- src/MusicCat.Service/Worker.cs
+++ src/MusicCat.Service/Worker.cs
@@ -1,104 +1,12 @@
-using ApiListener;
-using MusicCat.Metadata;
+using MusicCat.WebService;
 
 namespace MusicCat.Service;
 
-public class Worker(ILogger logger) : BackgroundService
+public class Worker : BackgroundService
 {
-    private const ApiLogLevel DisplayLogLevel = ApiLogLevel.Debug;
-
-    private FileStream? _logStream;
-    private StreamWriter? _logWriter;
-    
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        try
-        {
-            Listener.Config = Config.ParseConfig(out _logStream, out _logWriter);
-        }
-        catch
-        {
-            Listener.Config = Config.DefaultConfig;
-        }
-
-        try
-        {
-            MetadataStore.LoadMetadata(logger);
-        }
-        catch (Exception e)
-        {
-            if (_logStream != null)
-            {
-                _logWriter?.WriteLine(
-                    $"{Enum.GetName(typeof(ApiLogLevel), ApiLogLevel.Critical)?.ToUpper()}: Failed to load metadata. Exception: {e.Message}{Environment.NewLine}{e.StackTrace}");
-                _logWriter?.Flush();
-            }
-            Environment.Exit(1);
-        }
-
-        Listener.AttachLogger(m =>
-        {
-            if (m.Level >= DisplayLogLevel)
-            {
-                if (_logStream != null)
-                {
-                    _logWriter?.WriteLine($"{Enum.GetName(typeof(ApiLogLevel), m.Level)?.ToUpper()}: {m.Message}");
-                    _logWriter?.Flush();
-                }
-            }
-        });
-
-        var watcher = new FileSystemWatcher(Environment.CurrentDirectory, "MusicCatConfig.json")
-        {
-            NotifyFilter = NotifyFilters.LastWrite
-        };
-        watcher.Changed += OnConfigChange;
-        watcher.EnableRaisingEvents = true;
-
-        Listener.Start();
-        await Task.CompletedTask;
-    }
-    
-    private static int counter;
-    private void OnConfigChange(object sender, FileSystemEventArgs e)
-    {
-        if (counter == 0)
-        {
-            counter++;
-            return;
-        }
-
-        counter = 0;
-        Listener.Stop();
-        try
-        {
-            _logWriter?.Dispose();
-            _logStream?.Dispose();
-            Listener.Config = Config.ParseConfig(out _logStream, out _logWriter);
-        }
-        catch
-        { }
-        Listener.Start();
-        try
-        {
-            MetadataStore.LoadMetadata(logger);
-        }
-        catch (Exception ex)
-        {
-            if (_logStream != null)
-            {
-                _logWriter?.WriteLine(
-                    $"{Enum.GetName(typeof(ApiLogLevel), ApiLogLevel.Critical)?.ToUpper()}: Failed to load metadata. Exception: {ex.Message}{Environment.NewLine}{ex.StackTrace}");
-                _logWriter?.Flush();
-            }
-        }
-    }
-
-    public override Task StopAsync(CancellationToken cancellationToken)
-    {
-        Listener.Stop();
-        _logWriter?.Dispose();
-        _logStream?.Dispose();
-        return base.StopAsync(cancellationToken);
+        var app = MusicCatWebService.BuildWebApplication([]);
+        await app.RunAsync(stoppingToken);
     }
 }
---
+++ src/MusicCat.WebService/MusicCatWebService.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using MusicCat.Model;
+
+namespace MusicCat.WebService;
+
+public static class MusicCatWebService
+{
+    public static WebApplication BuildWebApplication(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.ConfigureHttpJsonOptions(options =>
+        {
+            // Enums as lowercase strings, so e.g. SongType.Betting becomes "betting".
+            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter(
+                namingPolicy: JsonNamingPolicy.SnakeCaseLower, allowIntegerValues: false));
+        });
+
+        WebApplication app = builder.Build();
+        var logger = app.Services.GetService<ILoggerFactory>()!.CreateLogger("MusicCatWebService");
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        // TODO hook up config
+        var musicLibrary = new MusicLibrary(
+            app.Services.GetService<ILogger<MusicLibrary>>()!,
+            musiclibraryPath: "S:/projects/musicLibrary",
+            songfilesPath: "V:/musiclibrary");
+        var loadTask = musicLibrary.Load(); // Start loading asynchronously, we don't need to wait for this to finish.
+        loadTask.ContinueWith(task => // But if it fails, we want to know about it
+        {
+            if (task.IsFaulted) logger.LogError(task.Exception, "Music Library load faulted");
+        });
+
+        app.MapGet("/musiclibrary/verify", async (bool reportUnusedSongFiles = false) =>
+            {
+                IList<string> warnings = await musicLibrary.Verify(reportUnusedSongFiles);
+                return string.Join('\n', warnings);
+            }).WithDescription("Like /reload, but a dry-run. So it just returns all problems that would occur")
+            .WithOpenApi();
+
+        app.MapGet("/musiclibrary/reload", async () =>
+            {
+                IList<string> warnings = await musicLibrary.Load();
+                return string.Join('\n', warnings);
+            }).WithDescription("Reloads the entire music library from disk, returning all problems that occurred")
+            .WithOpenApi();
+
+        app.MapGet("/musiclibrary/count", async (string? category) =>
+            {
+                if (category == null)
+                    return await musicLibrary.Count(null);
+                if (!Enum.TryParse(category, ignoreCase: true,
+                        out SongType songType)) // parse manually for case-insensitivity
+                    throw new BadHttpRequestException("Unrecognized song type: " + category);
+                return await musicLibrary.Count(songType);
+            }).WithDescription("Counts all currently enabled songs in the library, optionally filtered to one type")
+            .WithOpenApi();
+
+        return app;
+    }
+}
